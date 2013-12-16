b192a4701f7b506e181179542d467704d4990f69
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-12-16 06:56:29 -0500

    I knew that wouldn't work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :add_xframe
-
-  def add_xframe
-    headers['X-Frame-Options'] = 'GOFORIT'
-  end
-
 end
